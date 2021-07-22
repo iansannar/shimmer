f8150bfda4da26730b8c9044b5fe9f506513aaa2
Ian Sannar <ian.sannar@gmail.com>
2021-07-22 14:16:29 -0600

    :package: deploy 0.0.5-alpha

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("C:\\Users\\Ian\\Documents\\repos\\remedy\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("C:\\Users\\Ian\\Documents\\repos\\remedy\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("C:\\Users\\Ian\\Documents\\repos\\remedy\\node_modules\\remark-admonitions\\styles\\infima.css"),
+  require("C:\\Users\\Ian\\Documents\\repos\\remedy\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\Ian\\Documents\\repos\\remedy\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\Ian\\Documents\\repos\\remedy\\src\\css\\custom.css"),
+];
