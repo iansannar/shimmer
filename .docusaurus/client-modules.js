31d4e914b6ced004e443b687b3c484d120218d39
Ian Sannar <ian.sannar@gmail.com>
2021-10-06 00:29:33 -0600

    :package: deploy 0.0.6-alpha

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("C:\\Users\\Ian\\Documents\\repos\\remedy\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\Ian\\Documents\\repos\\remedy\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\Ian\\Documents\\repos\\remedy\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\Ian\\Documents\\repos\\remedy\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\Ian\\Documents\\repos\\remedy\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\Ian\\Documents\\repos\\remedy\\src\\css\\custom.css"),
-];
---
+++ assets/js/b2f554cd.eb06ddc8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkremedy=self.webpackChunkremedy||[]).push([[477],{10:function(e){e.exports={blogPosts:[]}}}]);
